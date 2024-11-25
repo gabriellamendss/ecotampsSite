8803787ba156861da86395bb7e7163d65232a6e0
Gabs <gabriellamendesdutra@gmail.com>
2024-11-25 15:50:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,23 +15,4 @@ for (i = 0; i < acc.length; i++) {
       pannel.style.display = "block";
     }
   });
-}
-
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        this.parentElement.classList.toggle("active");
-
-        var pannel = this.nextElementSibling;
-
-        if (pannel.style.display === "block") {
-          pannel.style.display = "none";
-        } else {
-          pannel.style.display = "block";
-        }
-      });
-    }
 //FIM DO FAQ
